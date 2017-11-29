57cd2fa9a8763d591cd27cb522df686a5623561c
catarinapessoajorge <chatarina_jorge@hotmail.com>
2017-11-29 16:18:15 +0000

    add exercice4

--- exercise4.py
+++ exercise4.py
@@ -1,3 +1,27 @@
 #Projecto Part2
 
-import xml.etree.ElementTree as ET
+#import xml.etree.ElementTree as ET
+
+from xml.etree import cElementTree as ET
+xmlstr = """
+    <root>
+    <page>
+    <title>Chapter 1</title>
+    <content>Welcome to Chapter 1</content>
+    </page>
+    <page>
+    <title>Chapter 2</title>
+    <content>Welcome to Chapter 2</content>
+    </page>
+    </root>
+    """
+root = ET.fromstring(xmlstr)
+#root = ET.fromstring(xmlstr)
+for item in list(root):
+    title = item.find('items:title')
+    #    name = item.get('name')
+    print(item.text)
+#print(title, name)
+#title = page.find('title').text
+#content = page.find('descritption').text
+#print(title)
