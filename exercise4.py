5f18eb4da8422f82ac88d401f600f627c4272f44
MrMSantos <manuel.jss@hotmail.com>
2017-11-29 17:21:46 +0000

    ex4 updated

--- exercise4.py
+++ exercise4.py
@@ -1,27 +1,12 @@
 #Projecto Part2
 
-#import xml.etree.ElementTree as ET
-
 from xml.etree import cElementTree as ET
-xmlstr = """
-    <root>
-    <page>
-    <title>Chapter 1</title>
-    <content>Welcome to Chapter 1</content>
-    </page>
-    <page>
-    <title>Chapter 2</title>
-    <content>Welcome to Chapter 2</content>
-    </page>
-    </root>
-    """
-root = ET.fromstring(xmlstr)
-#root = ET.fromstring(xmlstr)
-for item in list(root):
-    title = item.find('items:title')
-    #    name = item.get('name')
-    print(item.text)
-#print(title, name)
-#title = page.find('title').text
-#content = page.find('descritption').text
-#print(title)
+
+tree = ET.parse('CNN.rss')
+root = tree.getroot()
+
+for items in root.iter('item'):
+    title = items.find('title').text
+    description = items.find('description').text
+    print('TITLE:', title)
+    print('DESCRIPTION:', description)
